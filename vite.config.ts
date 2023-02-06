870e1125e7501b93a6005eb7b90c0649bd76917a
疒奀 <46226846+mlegls@users.noreply.github.com>
2023-02-06 04:23:39 -0800

    remove tauri

--- vite.config.ts
+++ vite.config.ts
@@ -9,19 +9,13 @@ export default defineConfig({
   // prevent vite from obscuring rust errors
   clearScreen: false,
   // tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true,
-  },
-  // to make use of `TAURI_DEBUG` and other env variables
-  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
-  envPrefix: ["VITE_", "TAURI_"],
+  envPrefix: ["VITE_"],
   build: {
     // Tauri supports es2021
-    target: ["es2021", "chrome100", "safari13"],
+    // target: ["es2021", "chrome100", "safari13"],
     // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+    // minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
     // produce sourcemaps for debug builds
-    sourcemap: !!process.env.TAURI_DEBUG,
+    // sourcemap: !!process.env.TAURI_DEBUG,
   },
 });
